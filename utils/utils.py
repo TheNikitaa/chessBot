ac833d34a1a6407cb1f71024504cc40261084f09
Chill320 <145222160+Chill320@users.noreply.github.com>
2024-01-28 22:09:53 +0300

    Delete utils.py

--- utils/utils.py
+++
@@ -1,7 +0,0 @@
-from stockfish import Stockfish
-
-def load_stockfish(strength, depth):
-    stockfish = Stockfish()
-    stockfish.set_skill_level(strength)
-    stockfish.set_depth(depth)
-    return stockfish
