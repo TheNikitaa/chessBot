7646724899fa9d3f731cecafff7621b46ae6e850
Nikita Akimov <nikitaamogus@gmail.com>
2024-02-15 19:46:28 +0300

    попытка номер 15

--- config/cfg.py
+++ config/cfg.py
@@ -1,1 +1,22 @@
-print(1)
+from dataclasses import dataclass
+from environs import Env
+
+@dataclass
+class TgBot:
+    token: str
+    admin_id: int
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+def load_config() -> Config:
+    env: Env = Env()
+    env.read_env()
+
+    return Config(
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN'),
+            admin_id=env('ADMIN_ID')
+        )
+    )
--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,45 @@
-print(1)
+from aiogram import types
+
+def start_kb() -> types.ReplyKeyboardMarkup:
+    kb = [
+        [
+            types.KeyboardButton(text="/settings"),
+            types.KeyboardButton(text="/game"),
+            types.KeyboardButton(text="Об авторах")
+        ]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+    return keyboard
+
+def settings_kb() -> types.ReplyKeyboardMarkup:
+    kb = [
+        [
+            types.KeyboardButton(text="/settings Easy"),
+            types.KeyboardButton(text="/settings Medium"),
+            types.KeyboardButton(text="/settings Hard")
+        ]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+    return keyboard
+
+def game_kb() -> types.ReplyKeyboardMarkup:
+    kb = [
+        [
+            types.KeyboardButton(text="/game White"),
+            types.KeyboardButton(text="/game Black")
+        ]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+    return keyboard
